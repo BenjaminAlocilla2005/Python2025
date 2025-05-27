ea30c2ed64e3d03425ed5ab9d05a1e5f2b931227
BenjaminAlocilla2005 <168034814+BenjaminAlocilla2005@users.noreply.github.com>
2025-05-27 10:09:20 -0400

    Add files via upload

---
+++ 1# Declarando variable con x = y.py
@@ -0,0 +1,35 @@
+name = "Benja"
+apellido = "Alocilla"
+edad = 19
+
+print("1. Hola soy", name)
+
+print(f"2. Hola mi nombre es {name}")
+
+print("3. ¡Hola buena! yo soy " + name + " " + apellido + " y tengo " + " " + str(edad) + " " + "años")
+
+
+edad = 19
+cumpleaños = 2005
+estatura = 1.89
+peso = 70.8
+
+
+n_comp = 5 + 4j
+n_comp = complex(5,4)
+
+print("4. mi edad es de: " + str(edad) + " años y mi estatura es de : " + str(estatura) + " M. " + "Y peso: " + str(peso) + " Kg.")
+
+pi = 3.4593
+print("El valor de pi es: ", round(pi,2))
+
+print("El valor de pi es; {:.4f}".format(pi))
+
+pais, region, ciudad, codigo_postal = "Chile,", "Los Lagos, ", "Castro ", 5780000
+print("5. La informacion de Benjamin: ", pais, region, ciudad, codigo_postal)
+
+year = input("6. (pregunta) en que año naciste? ")
+print("El año de nacimiento es: " + year)
+
+
+
