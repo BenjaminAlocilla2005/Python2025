8b86d1e1b69c0629a71c05617d521d6a7436f47e
BenjaminAlocilla2005 <168034814+BenjaminAlocilla2005@users.noreply.github.com>
2025-06-22 23:07:36 -0400

    Add files via upload

---
+++ Challenge1.py
@@ -0,0 +1,6 @@
+#primero se pone el nombre, la edad y luego la sumamos con 20 años
+nombre = "Benjamin"
+edad = 19
+total_edad = edad + 20
+
+print(f"Hola mi nombre es {nombre}, tengo {edad} años y en 20 años tendré {total_edad} años")
